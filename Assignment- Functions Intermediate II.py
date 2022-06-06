bcc25a3824aecacd8426ada79cf97308bc798672
Norah Alghomayjan <noor.workspace@gmail.com>
2022-06-06 16:18:06 +0300

    commit

--- Assignment- Functions Intermediate II.py
+++ Assignment- Functions Intermediate II.py
@@ -27,3 +27,40 @@ z = [ {'x': 10, 'y': 20} ]
 z[0]['y'] = 30
 print(z)
 
+# 2. Iterate Through a List of Dictionaries
+# Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value. For example, given the following list:
+
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary(students):
+    for i in students:
+        print( "first_name - " + i['first_name'] + ", last_name -" + i['last_name'])
+iterateDictionary(students)
+
+# 3. Get Values From a List of Dictionaries
+# Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries and a key name, the function prints the value stored in that key for each dictionary.
+
+def iterateDictionary2(key_name, some_list):
+    for i in some_list:
+        print (i[key_name])
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
+
+# Iterate Through a Dictionary with List Values
+# Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list. For example:
+
+dojo = {
+'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(some_dict):
+    for (key,value) in dojo.items():
+        print(f"{len(value)} {key}")
+        for i in value:
+            print (i)
+
+printInfo(dojo)
